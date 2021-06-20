3783fc8fb815f2fc764a763248cdc9ac1a927326
Landon Smith <landonasmith@gmail.com>
2021-06-19 18:46:50 -0700

    Initial commit

---
+++ ernesto/src/pong.rs
@@ -0,0 +1,180 @@
+extern crate amethyst;
+
+use amethyst::core::timing::Time;
+use amethyst::{
+    assets::{AssetStorage, Handle, Loader},
+    core::transform::Transform,
+    ecs::{Component, DenseVecStorage},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+};
+
+pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const PADDLE_WIDTH: f32 = 4.0;
+
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
+pub const BALL_VELOCITY_X: f32 = 10.0;
+pub const BALL_VELOCITY_Y: f32 = 10.0;
+pub const BALL_RADIUS: f32 = 2.0;
+
+#[derive(PartialEq, Eq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+#[derive(Default)]
+pub struct Pong {
+    ball_spawn_timer: Option<f32>,
+    sprite_sheet_handle: Option<Handle<SpriteSheet>>,
+}
+
+pub struct Paddle {
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+pub struct Ball {
+    pub velocity: [f32; 2],
+    pub radius: f32,
+}
+
+impl Paddle {
+    fn new(side: Side) -> Paddle {
+        Paddle {
+            side,
+            width: PADDLE_WIDTH,
+            height: PADDLE_HEIGHT,
+        }
+    }
+}
+
+impl SimpleState for Pong {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        // Wait one second before spawning the ball.
+        self.ball_spawn_timer.replace(1.0);
+
+        // Load the spritesheet necessary to render the graphics.
+        self.sprite_sheet_handle.replace(load_sprite_sheet(world));
+        initialize_paddles(world, self.sprite_sheet_handle.clone().unwrap());
+        initialize_camera(world);
+    }
+
+    fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        if let Some(mut timer) = self.ball_spawn_timer.take() {
+            // If the timer isn't expired yet, subtract the time that passed since the last update.
+            {
+                let time = data.world.fetch::<Time>();
+                timer -= time.delta_seconds();
+            }
+            if timer <= 0.0 {
+                // When timer expire, spawn the ball
+                initialize_ball(data.world, self.sprite_sheet_handle.clone().unwrap());
+            } else {
+                // If timer is not expired yet, put it back onto the state.
+                self.ball_spawn_timer.replace(timer);
+            }
+        }
+        Trans::None
+    }
+}
+
+impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
+
+impl Component for Ball {
+    type Storage = DenseVecStorage<Self>;
+}
+
+fn initialize_camera(world: &mut World) {
+    // Setup camera in a way that our screen covers whole arena and (0, 0) is in the bottom left.
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
+}
+
+/// Initializes one paddle on the left, and one paddle on the right.
+fn initialize_paddles(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+    let mut left_transform = Transform::default();
+    let mut right_transform = Transform::default();
+
+    // Assign the sprites for the paddles
+    let sprite_render = SpriteRender::new(sprite_sheet_handle, 0); // paddle is the first sprite in the sprite_sheet
+
+    // Correctly position the paddles.
+    let y = ARENA_HEIGHT / 2.0;
+    left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+    right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+
+    // Create a left plank entity.
+    world
+        .create_entity()
+        .with(sprite_render.clone())
+        .with(Paddle::new(Side::Left))
+        .with(left_transform)
+        .build();
+
+    // Create right plank entity.
+    world
+        .create_entity()
+        .with(sprite_render)
+        .with(Paddle::new(Side::Right))
+        .with(right_transform)
+        .build();
+}
+
+/// Initializes one ball in the middle-ish of the arena.
+fn initialize_ball(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+    // Create the translation.
+    let mut local_transform = Transform::default();
+    local_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+
+    // Assign the sprite for the ball. The ball is the second sprite in the sheet.
+    let sprite_render = SpriteRender::new(sprite_sheet_handle, 1);
+
+    world
+        .create_entity()
+        .with(sprite_render)
+        .with(Ball {
+            radius: BALL_RADIUS,
+            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
+        })
+        .with(local_transform)
+        .build();
+}
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    // Load the sprite sheet necessary to render the graphics.
+    // The texture is the pixel data
+    // `texture_handle` is a cloneable reference to the texture
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/pong_spritesheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/pong_spritesheet.ron", // Here we load the associated ron file
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    )
+}
---
+++ ping/src/main.rs
@@ -0,0 +1,58 @@
+mod pong;
+mod systems;
+
+use crate::pong::Pong;
+
+use amethyst::{
+    prelude::*,
+    core::transform::TransformBundle,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+    input::{InputBundle, StringBindings},
+};
+
+
+fn main() -> amethyst::Result<()> {
+
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle = InputBundle::<StringBindings>::new()
+        .with_bindings_from_file(binding_path)?;
+    
+    let game_data = GameDataBuilder::default()
+    .with_bundle(TransformBundle::new())?
+    .with_bundle(input_bundle)?
+    .with(systems::PaddleSystem, "paddle_system", &["input_system"]) // Add this line
+    .with(systems::MoveBallsSystem, "ball_system", &[])
+    .with(systems::WinnerSystem, "winner_system", &["ball_system"]);
+    .with(
+        systems::BounceSystem,
+        "collision_system",
+        &["paddle_system", "ball_system"],
+    )
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
+    game.run();
+
+
+    Ok(())
+}
